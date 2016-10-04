60fe9a0fea7fe71ca1e356420d4528ec682e5dee
haoyangqian <haoyang_qian@brown.edu>
2016-10-04 16:37:25 -0400

    parseIp

--- src/model/IpPacket.go
+++ src/model/IpPacket.go
@@ -10,17 +10,17 @@ type IpPacket struct {
 	Payload  []byte
 }
 
-func MakeIpPacket(message []byte,protocol int,src VirtualIp,dst VirtualIp) IpPacket {
+func MakeIpPacket(message []byte, protocol int, src VirtualIp, dst VirtualIp) IpPacket {
 	h := ipv4.Header{
 		Version:  4,
 		Len:      len(message),
-        TOS:      0,
-		TotalLen: 20 + len(message), 
+		TOS:      0,
+		TotalLen: 20 + len(message),
 		TTL:      16,
-		Protocol: protocol, 
-		Dst:      net.ParseIP(VirtualIp.Ip)
-        Src:      net.ParseIP(VirtualIp.Ip)
-        Options:  []byte{}
+		Protocol: protocol,
+		Dst:      net.ParseIP(VirtualIp.Ip),
+		Src:      net.ParseIP(VirtualIp.Ip),
+		Options:  []byte{},
 		// ID, Src and Checksum will be set for us by the kernel
 	}
 	return IpPacket(h, message)
