547b82b9db8a4ec9de5596599665c3b695efd246
Leo Sun <leo.tcsun@gmail.com>
2016-10-30 15:52:41 -0400

    Added a basic statmachine

---
+++ src/transport/TcpStateMachine.go
@@ -0,0 +1,178 @@
+package transport
+
+import (
+	"errors"
+)
+
+/*
+ * Constants
+ */
+var (
+	// EVENTS
+	TCP_ACTIVE_OPEN  TcpTransitionEvent = TcpTransitionEvent{ActiveOpen: true}
+	TCP_PASSIVE_OPEN TcpTransitionEvent = TcpTransitionEvent{PassiveOpen: true}
+	TCP_SEND         TcpTransitionEvent = TcpTransitionEvent{Send: true}
+	TCP_CLOSE        TcpTransitionEvent = TcpTransitionEvent{Close: true}
+	TCP_RECV_SYN     TcpTransitionEvent = TcpTransitionEvent{RecvSyn: true}
+	TCP_RECV_ACK     TcpTransitionEvent = TcpTransitionEvent{RecvAck: true}
+	TCP_RECV_FIN     TcpTransitionEvent = TcpTransitionEvent{RecvFin: true}
+	TCP_RECV_SYN_ACK TcpTransitionEvent = TcpTransitionEvent{RecvSyn: true, RecvAck: true}
+	TCP_TIMEOUT_2MSL TcpTransitionEvent = TcpTransitionEvent{Timeout: true}
+
+	// RESPONSES
+	TCP_RESP_DO_NOTHING   TcpTransitionResponse = TcpTransitionResponse{}
+	TCP_RESP_SEND_SYN     TcpTransitionResponse = TcpTransitionResponse{ShouldSendSyn: true}
+	TCP_RESP_SEND_ACK     TcpTransitionResponse = TcpTransitionResponse{ShouldSendAck: true}
+	TCP_RESP_SEND_FIN     TcpTransitionResponse = TcpTransitionResponse{ShouldSendFin: true}
+	TCP_RESP_SEND_SYN_ACK TcpTransitionResponse = TcpTransitionResponse{ShouldSendSyn: true, ShouldSendAck: true}
+	TCP_RESP_DEL_SOCK     TcpTransitionResponse = TcpTransitionResponse{ShouldDeleteSocket: true}
+
+	// STATES
+	// establish connection
+	TCP_INITIAL_CLOSED TcpState = TcpState{Name: "CLOSED"}
+	TCP_LISTEN         TcpState = TcpState{Name: "LISTEN"}
+	TCP_SYN_RCVD       TcpState = TcpState{Name: "SYN_RCVD"}
+	TCP_SYN_SENT       TcpState = TcpState{Name: "SYN_SENT"}
+	TCP_ESTAB          TcpState = TcpState{Name: "ESTAB"}
+
+	// active close
+	TCP_FIN_WAIT_1 TcpState = TcpState{Name: "FIN_WAIT_1", IsActiveClose: true}
+	TCP_FIN_WAIT_2 TcpState = TcpState{Name: "FIN_WAIT_2", IsActiveClose: true}
+	TCP_CLOSING    TcpState = TcpState{Name: "CLOSING", IsActiveClose: true}
+	TCP_TIME_WAIT  TcpState = TcpState{Name: "TIME_WAIT", IsActiveClose: true}
+
+	// passive close
+	TCP_CLOSE_WAIT   TcpState = TcpState{Name: "CLOSE_WAIT", IsPassiveClose: true}
+	TCP_LAST_ACK     TcpState = TcpState{Name: "LAST_ACK", IsPassiveClose: true}
+	TCP_FINAL_CLOSED TcpState = TcpState{Name: "CLOSED", IsPassiveClose: true}
+)
+
+type TcpTransitionEvent struct {
+	ActiveOpen  bool
+	PassiveOpen bool
+	Send        bool
+	Close       bool
+	RecvSyn     bool
+	RecvAck     bool
+	RecvFin     bool
+	Timeout     bool
+}
+
+func MakeTcpTransitionEvent(header TCPHeader) TcpTransitionEvent {
+	recvSyn, recvAck, recvFin := false, false, false
+	if header.HasFlag(SYN) {
+		recvSyn = true
+	}
+
+	if header.HasFlag(ACK) {
+		recvAck = true
+	}
+
+	if header.HasFlag(FIN) {
+		recvFin = true
+	}
+
+	return TcpTransitionEvent{RecvSyn: recvSyn, RecvAck: recvAck, RecvFin: recvFin}
+}
+
+type TcpTransitionResponse struct {
+	ShouldSendSyn      bool
+	ShouldSendAck      bool
+	ShouldSendFin      bool
+	ShouldDeleteSocket bool
+}
+
+func (r *TcpTransitionResponse) ShouldDoNothing() bool {
+	return r.ShouldSendSyn || r.ShouldSendAck || r.ShouldSendFin || r.ShouldDeleteSocket
+}
+
+func (r *TcpTransitionResponse) GetCtrlFlags() int {
+	flags := 0
+	if r.ShouldSendSyn {
+		flags = flags | SYN
+	}
+
+	if r.ShouldSendAck {
+		flags = flags | ACK
+	}
+
+	if r.ShouldSendFin {
+		flags = flags | FIN
+	}
+
+	return flags
+}
+
+type TcpState struct {
+	Name           string
+	IsActiveClose  bool
+	IsPassiveClose bool
+}
+
+type TcpTransition struct {
+	CurrentState TcpState
+	Event        TcpTransitionEvent
+}
+
+type TcpStateMachine struct {
+	currentState TcpState
+	states       map[TcpTransition]TcpState
+	responses    map[TcpTransition]TcpTransitionResponse
+}
+
+func (m *TcpStateMachine) CurrentState() TcpState {
+	return m.currentState
+}
+
+func (m *TcpStateMachine) HasTransition(event TcpTransitionEvent) bool {
+	transition := TcpTransition{m.CurrentState(), event}
+	if _, ok := m.states[transition]; ok {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (m *TcpStateMachine) GetResponse(event TcpTransitionEvent) (TcpTransitionResponse, error) {
+	var response TcpTransitionResponse
+	if !m.HasTransition(event) {
+		return response, errors.New("Statemachine does not have transition for the given input event")
+	}
+
+	transition := TcpTransition{m.CurrentState(), event}
+	return m.responses[transition], nil
+}
+
+func (m *TcpStateMachine) Transit(event TcpTransitionEvent) error {
+	if !m.HasTransition(event) {
+		return errors.New("Statemachine does not have transition for the given input event")
+	}
+
+	transition := TcpTransition{m.CurrentState(), event}
+	m.currentState = m.states[transition]
+
+	return nil
+}
+
+type TcpStateMachineBuilder struct {
+	initialState TcpState
+	states       map[TcpTransition]TcpState
+	responses    map[TcpTransition]TcpTransitionResponse
+}
+
+func MakeTcpStateMachineBuilder(initialState TcpState) TcpStateMachineBuilder {
+	states := make(map[TcpTransition]TcpState)
+	responses := make(map[TcpTransition]TcpTransitionResponse)
+
+	return TcpStateMachineBuilder{initialState, states, responses}
+}
+
+func (b *TcpStateMachineBuilder) RegisterTransition(fromState TcpState, event TcpTransitionEvent, response TcpTransitionResponse, toState TcpState) {
+	transition := TcpTransition{fromState, event}
+	b.states[transition] = toState
+	b.responses[transition] = response
+}
+
+func (b *TcpStateMachineBuilder) Build() TcpStateMachine {
+	return TcpStateMachine{b.initialState, b.states, b.responses}
+}
---
+++ src/transport/TcpStateMachineTest.go
@@ -0,0 +1,96 @@
+package transport
+
+import (
+	"fmt"
+)
+
+func TestStateMachine() {
+	builder := MakeTcpStateMachineBuilder(TCP_INITIAL_CLOSED)
+
+	builder.RegisterTransition(TCP_INITIAL_CLOSED, TCP_PASSIVE_OPEN, TCP_RESP_DO_NOTHING, TCP_LISTEN)
+
+	builder.RegisterTransition(TCP_LISTEN, TCP_CLOSE, TCP_RESP_DEL_SOCK, TCP_INITIAL_CLOSED)
+	builder.RegisterTransition(TCP_LISTEN, TCP_RECV_SYN, TCP_RESP_SEND_SYN_ACK, TCP_SYN_RCVD)
+	builder.RegisterTransition(TCP_LISTEN, TCP_SEND, TCP_RESP_SEND_SYN, TCP_SYN_SENT)
+
+	builder.RegisterTransition(TCP_SYN_RCVD, TCP_CLOSE, TCP_RESP_SEND_FIN, TCP_FIN_WAIT_1)
+	builder.RegisterTransition(TCP_SYN_RCVD, TCP_RECV_ACK, TCP_RESP_DO_NOTHING, TCP_ESTAB)
+
+	builder.RegisterTransition(TCP_SYN_SENT, TCP_CLOSE, TCP_RESP_DEL_SOCK, TCP_INITIAL_CLOSED)
+	builder.RegisterTransition(TCP_SYN_SENT, TCP_RECV_SYN, TCP_RESP_SEND_SYN_ACK, TCP_SYN_RCVD)
+	builder.RegisterTransition(TCP_SYN_SENT, TCP_RECV_SYN_ACK, TCP_RESP_SEND_ACK, TCP_ESTAB)
+
+	builder.RegisterTransition(TCP_ESTAB, TCP_CLOSE, TCP_RESP_SEND_FIN, TCP_FIN_WAIT_1)
+	builder.RegisterTransition(TCP_ESTAB, TCP_RECV_FIN, TCP_RESP_SEND_ACK, TCP_CLOSE_WAIT)
+
+	builder.RegisterTransition(TCP_FIN_WAIT_1, TCP_RECV_ACK, TCP_RESP_DO_NOTHING, TCP_FIN_WAIT_2)
+	builder.RegisterTransition(TCP_FIN_WAIT_1, TCP_RECV_FIN, TCP_RESP_SEND_ACK, TCP_CLOSING)
+
+	builder.RegisterTransition(TCP_FIN_WAIT_2, TCP_RECV_FIN, TCP_RESP_SEND_ACK, TCP_TIME_WAIT)
+
+	builder.RegisterTransition(TCP_CLOSING, TCP_RECV_ACK, TCP_RESP_DO_NOTHING, TCP_TIME_WAIT)
+
+	builder.RegisterTransition(TCP_TIME_WAIT, TCP_TIMEOUT_2MSL, TCP_RESP_DEL_SOCK, TCP_FINAL_CLOSED)
+
+	builder.RegisterTransition(TCP_CLOSE_WAIT, TCP_CLOSE, TCP_RESP_SEND_FIN, TCP_LAST_ACK)
+
+	builder.RegisterTransition(TCP_LAST_ACK, TCP_RECV_ACK, TCP_RESP_DO_NOTHING, TCP_FINAL_CLOSED)
+
+	machine := builder.Build()
+
+	machine.CurrentState()
+	var r TcpTransitionResponse
+
+	fmt.Printf("Starting state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("Passive OPEN, expecte next state to be LISTEN\n")
+	machine.Transit(TCP_PASSIVE_OPEN)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("CLOSE, expect next state to be CLOSED\n")
+	machine.Transit(TCP_CLOSE)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("Passive OPEN, expecte next state to be LISTEN\n")
+	machine.Transit(TCP_PASSIVE_OPEN)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("SEND, expect next state to be SYN_SENT\n")
+	machine.Transit(TCP_SEND)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("rcv SYN, expect next state to be SYN_RCVD\n")
+	if !machine.HasTransition(TCP_RECV_SYN) {
+		panic("state LISTEN cannot transit with TCP_RECV_SYN event")
+	}
+	r, _ = machine.GetResponse(TCP_RECV_SYN)
+	fmt.Printf("Response to TCP_RECV_SYN event is %+v, ctrl flags should be %b\n", r, r.GetCtrlFlags())
+	machine.Transit(TCP_RECV_SYN)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("rcv ACK, expect next state to be ESTAB\n")
+	machine.Transit(TCP_RECV_ACK)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("CLOSE, expect next state to be FIN_WAIT_1\n")
+	machine.Transit(TCP_CLOSE)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("rcv ACK, expect next state to be FIN_WAIT_2\n")
+	machine.Transit(TCP_RECV_ACK)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+
+	fmt.Printf("rcv FIN, expect next state to be TIME_WAIT\n")
+	machine.Transit(TCP_RECV_FIN)
+	fmt.Printf("Current state is %s\n", machine.CurrentState().Name)
+	fmt.Printf("\n\n")
+}
